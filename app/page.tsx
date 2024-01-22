954b705f37cc4c0f7d9ef496e7da0099d5eb4c2e
yangxdev <janyxiang@deloitte.it>
2024-01-22 11:37:23 +0100

    Card max width adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MainSection from '../app/components/MainSection';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col py-10 px-24 select-none">
+        <main className="flex w-full max-w-screen-xl flex-col py-10 px-24 select-none">
             <Header />
             <hr 
                 className="mt-6 mb-8 
